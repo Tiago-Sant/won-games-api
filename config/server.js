d7f596d12a18a6e9292aea644b374b52b1d9d0ad
Tiago-Sant <tiagocsantana10@gmail.com>
2021-09-21 12:37:19 -0300

    Api Strappi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '277cd61c2950017485980c233e8dca91'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a331d661-f1cb-4d20-9037-3e57f2c9e51f'
+};
